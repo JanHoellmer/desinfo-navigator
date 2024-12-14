f8b20de61118a63e2017a3090e82fe7256f4d2c7
Jan Höllmer <jan.hoellmer@scieneers.de>
2024-12-14 16:46:19 +0100

    implemented placeholder for first message

--- pipelines/desinfo.py
+++ pipelines/desinfo.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 from typing import List, Union, Generator, Iterator
 from schemas import OpenAIChatMessage
 from pydantic import BaseModel
 from openai import AzureOpenAI, RateLimitError
 import os
+from enum import Enum
 
 
 class Pipeline:
@@ -25,7 +28,7 @@ class Pipeline:
         self.llm = AzureOpenAI(
             max_retries=5,
             api_key=api_key,
-            azure_endpoint=endpoint,
+            azure_endpoint=endpoint
         )
 
     async def on_startup(self):
@@ -51,24 +54,93 @@ class Pipeline:
     def pipe(
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
-        # This is where you can add your custom pipelines like RAG.
-        print(f"pipe:{__name__}")
-
         # If you'd like to check for title generation, you can add the following check
         if body.get("title", False):
             print("Title Generation Request")
 
         if is_first_message(messages):
-            # do first thing -> ampel
+            strategies: list[AppliedStrategy] = identify_strategies(user_message)
+            result = AppliedStrategy.construct_answer_from_list(strategies=strategies, original_text=user_message, openai_client=self.llm)
+        else:
+            # follow up
             completion = self.llm.chat.completions.create(
                 model=self.deployment, messages=messages
             )
             result = completion.choices[0].message.content
-        else:
-            # follow up
-            result = "Das sind alle anderen Antworten"
 
         return result
     
+class Strategy(Enum):
+    FAKE_EXPERTS = "Pseudo-Experten"
+    LOGICAL_FALLACIES = "Logischer Trugschluss"
+    IMPOSSIBLE_EXPECTATIONS = "Unerfüllbare Erwartungen"
+    CHERRY_PICKING = "Rosinenpickerei"
+    CONSPIRACY_THEORIES = "Verschwörungsmythen"
+
+    def get_description(self) -> str:
+        if self == Strategy.FAKE_EXPERTS:
+            return "Eine unqualifizierte Person oder Institution wird als Quelle glaubwürdiger Informationen präsentiert."
+        elif self == Strategy.LOGICAL_FALLACIES:
+            return "Argumente, bei denen sich die Schlussfolgerung nicht logischerweise aus den Prämissen ergibt. Auch bekannt als Non-Sequitur."
+        elif self == Strategy.IMPOSSIBLE_EXPECTATIONS:
+            return "Unrealistische Standards der Sicherheit fordern, bevor man die Wissenschaft akzeptiert."
+        elif self == Strategy.CHERRY_PICKING:
+            return "Sorgfältige Auswahl von Daten, die eine Position zu bestätigen scheinen, während andere Daten ignoriert werden, die dieser Position widersprechen."
+        elif self == Strategy.CONSPIRACY_THEORIES:
+            return "Eine Verschwörung zur Umsetzung eines üblen Plans vermuten, wie das Verbergen der Wahrheit oder das Weitergeben von Falschinformationen."
+        else:
+            raise NotImplementedError(f"{self} not implemented.")
+    
+class AppliedStrategy(BaseModel):
+    content: str
+    offset: int
+    strategy: Strategy
+
+    @classmethod
+    def return_example_list(cls) -> list[AppliedStrategy]:
+        return [
+            AppliedStrategy(
+                content="Das Klima hat sich in der Vergangenheit wegen natürlichen Ursachen verändert, also haben die heutigen Veränderungen ebenfalls natürliche Ursachen.",
+                offset=0,
+                strategy=Strategy.LOGICAL_FALLACIES
+            ),
+            AppliedStrategy(
+                content="Wissenschaftler können nicht einmal das Wetter in der nächsten Woche vorhersagen. Wie sollen sie also das Wetter in 100 Jahren vorhersagen können?",
+                offset=0,
+                strategy=Strategy.IMPOSSIBLE_EXPECTATIONS
+            )
+        ]
+    
+    def create_action(self, original_text: str, openai_client: AzureOpenAI) -> str:
+        return "PLACEHOLDER_ACTION"
+    
+    def stringify_long(self, original_text: str, openai_client: AzureOpenAI) -> str:
+        return f"""\t- Definition: {self.strategy.get_description()}
+        \t- Passage: {self.content}
+        \t- Individueller Denkauftrag: {self.create_action(original_text=original_text, openai_client=openai_client)}"""
+    
+    def stringify_short(self) -> str:
+        return f"\t- {self.strategy.name}"
+
+    @classmethod
+    def construct_answer_from_list(cls, strategies: list[AppliedStrategy], original_text: str, openai_client: AzureOpenAI) -> str:
+        individual_strategies_long = "\n\n".join([strategy.stringify_long(original_text, openai_client) for strategy in strategies])
+        individual_strategies_short = "\n".join([strategy.stringify_short() for strategy in strategies])
+        return f"""
+        AMPEL
+
+        Es liegen folgende Merkmale von Desinformation vor
+        {individual_strategies_short}
+
+        Individuelle Anweisungen
+        {individual_strategies_long}
+        """
+    
+AppliedStrategy.model_rebuild()
+
+def identify_strategies(user_message: str) -> list[AppliedStrategy]:
+    """ identifies strategies from a user message """
+    return AppliedStrategy.return_example_list()
+
 def is_first_message(messages: list[dict]) -> bool:
     return len(messages) == 1
